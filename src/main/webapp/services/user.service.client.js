cdaa88b970b6bf5eeeb8077a6827c8e8613cf6c1
FengyiQuan <quan.f@husky.neu.edu>
2020-05-15 16:26:41 -0400

    services

--- src/main/webapp/services/user.service.client.js
+++ src/main/webapp/services/user.service.client.js
@@ -4,7 +4,7 @@ function AdminUserServiceClient() {
     this.findUserById = findUserById;
     this.deleteUser = deleteUser;
     this.updateUser = updateUser;
-    this.url = 'https://wbdv-generic-server.herokuapp.com/api/jannunzi/users';
+    this.url = 'https://wbdv-generic-server.herokuapp.com/api/001886456/users';
     var self = this;
 
     function createUser(user) {
