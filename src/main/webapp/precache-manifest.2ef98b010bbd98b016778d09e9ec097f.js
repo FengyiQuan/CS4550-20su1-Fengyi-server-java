bac8211f8b3570a9bc891a7e96eebe0a09c61d1a
FengyiQuan <quan.f@husky.neu.edu>
2020-05-26 14:15:54 -0400

    add react app

---
+++ src/main/webapp/precache-manifest.2ef98b010bbd98b016778d09e9ec097f.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "695d168e82548c506306c4b2f1b404c7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4f2a1f8acf8570e31b3a",
+    "url": "/static/css/2.ddfacf9b.chunk.css"
+  },
+  {
+    "revision": "4369fa07120333c339da",
+    "url": "/static/css/main.af975c72.chunk.css"
+  },
+  {
+    "revision": "4f2a1f8acf8570e31b3a",
+    "url": "/static/js/2.3c15332b.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.3c15332b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4369fa07120333c339da",
+    "url": "/static/js/main.5a8d35ef.chunk.js"
+  },
+  {
+    "revision": "ec02476805c95f4ccdd4",
+    "url": "/static/js/runtime-main.ca02b13f.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
